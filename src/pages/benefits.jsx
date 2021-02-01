18593a3c68ffd6cac559c20c0c6230d09eba3f30
Cas <testing@owners-MacBook-Pro-2.local>
2021-01-31 20:59:48 -0600

    Made edits to learnAbout and benefits pages.

--- src/pages/benefits.jsx
+++ src/pages/benefits.jsx
@@ -7,10 +7,10 @@ export default class Benefits extends React.Component {
                 <h1> Benefits of Meditation </h1>
                 <p>Meditation has many functions:</p>
                 <ul>
-<li>It overcomes inner problems such as those created by anger, jealousy, attachment, and ignorance</li>
-<li>It controls our mind and brings inner peace</li>
-<li>It enables us to cultivate virtuous intentions that lead us to perform good actions</li>
-<li>It eliminates non-virtuous intentions that lead us to perform harmful actions</li>
+<li> Empowers one to overcome inner problems such as those created by anger, jealousy, attachment, and ignorance</li>
+<li> Allows us to control our minds as well as an opportunity to bring us inner peace</li>
+<li> Enables us to cultivate virtuous intentions that lead us to perform good actions</li>
+<li> Eliminates non-virtuous intentions that lead us to perform harmful actions</li>
 </ul>
                 
 
