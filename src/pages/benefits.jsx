4d8fc1377d92584f464b075fe765ba48870ff7c4
Dan <dzhu0513@gmail.com>
2021-01-31 21:18:13 -0600

    text layout edits

--- src/pages/benefits.jsx
+++ src/pages/benefits.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default class Benefits extends React.Component {
     render() {
         return (
-            <div>
-                <h1> Benefits of Meditation </h1>
-                <p>Meditation has many functions:</p>
+            <div className = "learnContainer">
+                <h1 style = {{textAlign:"center"}}> Benefits of Meditation </h1>
+                <h2>Meditation has many functions:</h2>
                 <ul>
 <li> Empowers one to overcome inner problems such as those created by anger, jealousy, attachment, and ignorance</li>
 <li> Allows us to control our minds as well as an opportunity to bring us inner peace</li>
